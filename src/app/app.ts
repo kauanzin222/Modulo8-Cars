0565b05650deef85e01c5005dc9c3669efb79894
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-12-15 10:52:14 -0300

    closes #1 - Criando o componente header e atribuindo um título a ele

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header-component/header-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [HeaderComponent]
 })
 export class App {
-  protected readonly title = signal('modulo8-Cars');
+ 
 }
